9b1289584dbba4e14ec662a1e3e847f9a58d2041
ekayaint <ekayaint@gmail.com>
2023-11-10 08:49:15 +0300

    Initial Commit

---
+++ SwiftUI-MessengerApp-Sample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-MessengerApp-Sample
+//
+//  Created by ekayaint on 10.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-MessengerApp-Sample/SwiftUI_MessengerApp_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MessengerApp_SampleApp.swift
+//  SwiftUI-MessengerApp-Sample
+//
+//  Created by ekayaint on 10.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MessengerApp_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
